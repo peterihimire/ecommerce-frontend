9e02e89c249d9671d4f8809bab50be67f473874f
peterihimire <peterihimire@gmail.com>
2023-04-14 23:42:12 +0100

    Worked on the product card

--- src/components/ui/productCard/index.js
+++ src/components/ui/productCard/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import prodImg from "../../../assets/images/product8.png";
 
 import {
   ShoppingCartOutlined,
